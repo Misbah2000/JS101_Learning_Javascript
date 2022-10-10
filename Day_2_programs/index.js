cdbbbd78d81cda91275f42c2cd1ffa773e142785
Misba Mansuri <misbamansuri2000@gmail.log>
2022-10-10 22:17:56 +0530

    Programme 2 day code

---
+++ Day_2_programs/index.js
@@ -0,0 +1,13 @@
+let x = 3;
+console.log(x ** 0.5)
+console.log(1/2)
+console.log(2/3)
+
+let first_name= "misba"
+let last_name= "mansuri"
+console.log(first_name + last_name)
+
+let a = 10;
+let b = 2;
+console.log((a + b)**b);
+
